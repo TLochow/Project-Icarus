193235dd775d8167d648ecf7442fb060797bcfa4
Torben Lochow <t.lochow@gmx.de>
2020-05-01 15:00:25 +0200

    Basically done.

--- Asteroid.gd
+++ Asteroid.gd
@@ -1,6 +1,11 @@
 extends RigidBody2D
 
+var EXPLOSIONSCENE = preload("res://Explosion.tscn")
+
 signal Destroyed
+
+func _ready():
+	angular_velocity = rand_range(-30.0, 30.0)
 
 func _physics_process(delta):
 	var player = get_tree().get_nodes_in_group("Player")
@@ -9,7 +14,15 @@ func _physics_process(delta):
 		var pos = get_position()
 		
 		var directionToPlayer = playerPos - pos
-		var squareDistance = clamp(directionToPlayer.length_squared() * 0.00001, 0.1, 0.8)
+		var squareDistance = clamp(directionToPlayer.length_squared() * 0.000001, 0.1, 0.8)
 		var gravityDirection = directionToPlayer.normalized()
 		var gravityForce = gravityDirection * 2.0 / squareDistance
 		linear_velocity += gravityForce
+
+func _on_CollisionArea_body_entered(body):
+	if body != self:
+		var explosion = EXPLOSIONSCENE.instance()
+		explosion.set_position(get_position())
+		get_tree().get_root().add_child(explosion)
+		emit_signal("Destroyed")
+		queue_free()
--- Main.gd
+++ Main.gd
@@ -5,19 +5,32 @@ var ASTEROIDSCENE = preload("res://Asteroid.tscn")
 var Asteroids
 var Points
 
+var GameOver = false
+
+var PreviousShipPos
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	get_viewport().warp_mouse(Vector2(512, 300))
 	
 	Asteroids = 0
 	Points = 0
+	PreviousShipPos = Vector2(512.0, 300.0)
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		SceneChanger.EndGame()
+	elif GameOver:
+		if event.is_action_pressed("ui_accept"):
+			SceneChanger.ChangeScene("res://Main.tscn")
 
 func _process(delta):
-	$Player.set_position(.get_global_mouse_position())
+	if not GameOver:
+		var pos = .get_global_mouse_position()
+		$Player.set_position(pos)
+		var directionToBefore = PreviousShipPos - pos
+		$Player/Sprite.rotation_degrees = clamp(directionToBefore.x, -45.0, 45.0)
+		PreviousShipPos = pos
 
 func _on_SpawnTimer_timeout():
 	var asteroid = ASTEROIDSCENE.instance()
@@ -27,9 +40,21 @@ func _on_SpawnTimer_timeout():
 	asteroid.connect("Destroyed", self, "AsteroidDestroyed")
 	$Asteroids.add_child(asteroid)
 	Asteroids += 1
+	$UI/Asteroids.text = "Asteroids: " + str(Asteroids)
 
 func AsteroidDestroyed():
 	Asteroids -= 1
+	$UI/Asteroids.text = "Asteroids: " + str(Asteroids)
 
 func _on_PointsTimer_timeout():
-	Points += Asteroids
+	if not GameOver:
+		Points += Asteroids
+		$UI/Score.text = "Score: " + str(Points)
+
+func _on_Player_body_entered(body):
+	if not GameOver:
+		GameOver = true
+		$Player/Sprite.visible = false
+		$Player/Explosion.emitting = true
+		$UI/End/Score.text = "Score: " + str(Points)
+		$UI/End.visible = true
